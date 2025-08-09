4b66e3ece9a2585a09f6abbc8651a7f2cb80c192
theobadha <w.obadha@gmail.com>
2025-08-09 08:28:18 +0300

    implement cart page

---
+++ frontend/src/Cart.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Cart({ cart }) {
+  const navigate = useNavigate();
+  // If cart is empty, display a message
+  if (!cart || cart.length === 0) {
+    return <div>Your cart is empty. Start shopping!</div>;
+  }
+
+  //show cart items
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <ul>
+        {cart.map((item) => (
+          <li key={item._id}>
+            {item.itemName} - Qty: {item.quantity} - ${item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => navigate("/")}>Continue Shopping</button>
+    </div>
+  );
+}
+
+export default Cart;
